5f59a9bde1f3afbbb535d42d4bd1ac8759d0137e
Anthony Chiara <achiara12@gmail.com>
2019-08-06 16:47:57 -0400

    made subtle changes.

--- Program.cs
+++ Program.cs
@@ -10,18 +10,19 @@ namespace DecisionMaker
     {
         static void Main(string[] args)
         {
-                Console.WriteLine("Please enter name.");
-                string userName = Console.ReadLine();
+            Console.WriteLine("Please enter name.");
+            string userName = Console.ReadLine();
+            bool again = true;
+            while (again)
+            {
                 Console.WriteLine($"Welcome, {userName} , please enter from 1-100.");
                 string input = Console.ReadLine();
-                int number = int.Parse(input);
+                int number = int.Parse(input); //add if statement for this
 
-            
-            
                 if (number % 2 == 0 && number >= 2 && number < 25 || number == 25)
                 {
                     Console.WriteLine("Even and less than or equal to 25");
-                   
+
                 }
                 else if (number % 2 != 0 && number != 60)
                 {
@@ -31,11 +32,11 @@ namespace DecisionMaker
                 {
                     Console.WriteLine($"Even, {userName}");
                 }
-                else if (number % 2 == 0 && number >= 60 && number < 100 || number == 100)
+                else if (number % 2 == 0 && number >= 60 && number < 100 || number == 100) 
                 {
                     Console.WriteLine($"{number} even, {userName}");
                 }
-                else if (number % 2 != 0 && number >= 61 && number < 100 || number == 99)
+                else if ((number % 2 != 0) && (number >= 60) && (number < 100) || (number == 99))
 
                 {
                     Console.WriteLine($"{number} odd, {userName}");
@@ -46,8 +47,23 @@ namespace DecisionMaker
                     input = Console.ReadLine();
                     number = int.Parse(input);
                 }
-            
-            Console.ReadKey();
+                Console.WriteLine("Would you like to continue? (y/n)");
+                string userchoice = Console.ReadLine().ToLower();
+                if (userchoice == "y" || userchoice == "yes")
+                {
+                    again = true;
+                    
+                }
+                
+                else
+                {
+                    again = false;
+                    Console.WriteLine("\nFareWell.");
+
+                }
+                
+            }
+                
         }
     }
 }
